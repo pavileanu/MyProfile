a5995e926b13132e2478d9ff0dc7a3f9c1e422b5
pavileanu <mirceashark2007@yahoo.com>
2020-06-22 22:02:21 +0300

    test1

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ const pass = 'Jaolica1';
 
 const sendEmail = async options => {
     
-    try {
+    //try {
         const transporter = nodemailer.createTransport({
             host,
             port,
@@ -26,10 +26,12 @@ const sendEmail = async options => {
         }
 
         const response = await transporter.sendMail(mailOptions);
-        return 'e bine';
-    } catch (err) {
+        return true;
+    //} 
+    /*
+    catch (err) {
         return err.messsage;
-    }
+    }*/
 
 }
 
